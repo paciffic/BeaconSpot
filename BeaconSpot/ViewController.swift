e115358e01caf405acbc42366563de381a72ef2f
paciffic <paciffic@gmail>
2015-06-29 14:47:47 +0900

    Beacon Spot was added

--- BeaconSpot/ViewController.swift
+++ BeaconSpot/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import QuartzCore
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnSwitchSpotting: UIButton!
+    
+    @IBOutlet weak var lblBeaconReport: UILabel!
+    
+    @IBOutlet weak var lblBeaconDetails: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lblBeaconDetails.hidden = true
+        btnSwitchSpotting.layer.cornerRadius = 30.0
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    // MARK: IBAction method implementation
+    
+    @IBAction func switchSpotting(sender: AnyObject) {
+        
+    }
+    
 
 }
 
